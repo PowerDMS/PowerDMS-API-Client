5e58ed7b8d574e5a3340ea9499c07d43156bb729
Alex Denton <alex.denton.dev@gmail.com>
2017-11-04 20:01:28 -0400

    Added some extensions on HttpResponseMessage

---
+++ PowerDms.Api.Rest.Client/PowerDms.Api.Rest.Client/HttpResponseMessageExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PowerDms.Api.Rest.Client
+{
+    public static class HttpResponseMessageExtensions
+    {
+        public static async Task<T> GetContent<T>(this HttpResponseMessage httpResponseMessage)
+        {
+            var content = await httpResponseMessage.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+
+        public static async Task<T> AwaitGetSuccessfulResponse<T>(this Task<HttpResponseMessage> httpResponseMessageTask)
+        {
+            return await (await httpResponseMessageTask)
+                .GetSuccessfulResponse<T>();
+        }
+
+        public static async Task<T> GetSuccessfulResponse<T>(this HttpResponseMessage httpResponseMessage)
+        {
+            return await httpResponseMessage
+                .EnsureSuccessStatusCode()
+                .GetContent<T>();
+        }
+    }
+}
