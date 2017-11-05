9429a9e6dc36c80b807343915bf0082e7f9324d5
Alex Denton <alex.denton.dev@gmail.com>
2017-11-04 21:14:44 -0400

    Added OAuth client

---
+++ PowerDms.Api.Rest.Client/PowerDms.Api.Rest.Client/OAuthClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerDms.Api.Rest.Client
+{
+    public class OAuthClient
+    {
+        private readonly HttpClient _HttpClient;
+
+        public Task<HttpResponseMessage> GetAccessToken(
+            string username,
+            string password,
+            string siteKey,
+            string oauthClientSecret)
+        {
+            var httpRequestMessage = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post
+            };
+
+            httpRequestMessage.Headers.Authorization = 
+                new AuthenticationHeaderValue(
+                    "Basic",
+                    Convert.ToBase64String(
+                        Encoding.ASCII.GetBytes(oauthClientSecret)));
+
+            var content = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password),
+                new KeyValuePair<string, string>("scope", "openid profile tnt_profile privileges"),
+                new KeyValuePair<string, string>("acr_values", $"tenant:{siteKey}")
+            };
+
+            httpRequestMessage.Content = new FormUrlEncodedContent(content);
+
+            return _HttpClient.SendAsync(httpRequestMessage);
+        }
+    }
+}
--- PowerDms.Api.Rest.Client/PowerDms.Api.Rest.Client/RestApiRoutes.cs
+++ PowerDms.Api.Rest.Client/PowerDms.Api.Rest.Client/RestApiRoutes.cs
@@ -5,5 +5,7 @@
         public const string PowerDmsRestApiDomain = "api.powerdms.com";
 
         public const string Groups = "/principal/groups";
+
+        public const string OAuthAccessToken = "/auth/connect/token";
     }
 }
